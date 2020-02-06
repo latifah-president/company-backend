8f6f3b8eda744cc78dd46ecda47b7c46ca211d42
Latifah President <latifahpresident@Latifahs-MBP.lan>
2020-02-05 22:33:13 -0600

    beginning knex config

--- index.js
+++ index.js
@@ -1,10 +1,3 @@
-// require('dotenv');
-
-// const server = require('./app.js');
-// const PORT = process.env.PORT || 5050
-
-// server.listen(PORT, () => console.log(`server listening on ${PORT}`));
-
 require('dotenv');
 const server = require('./app');
 const PORT = process.env.PORT || 5000
